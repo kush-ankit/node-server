7c8e563eb7d1c4d03bdc7c66df93ce0b66135012
kush-ankit <er.ankitkush@gmail.com>
2025-02-04 15:19:47 +0530

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log("GET request");
+    res.json({url:"https://www.google.com/"})
+});
+
+app.listen(port, '192.168.1.56', () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
