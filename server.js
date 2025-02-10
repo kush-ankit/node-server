13b18a2b10f6a3e4262f36796481c5456b3c745b
kush-ankit <er.ankitkush@gmail.com>
2025-02-11 01:43:08 +0530

    card

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    const phone = {
-        name: 'iPhone 15 Pro Max',
-        price: '1199',
-        image: 'https://www.silicon.co.uk/wp-content/uploads/2023/09/Apple-iPhone-15-Pro-lineup-02.jpg',
-        buyLink: 'https://www.apple.com/shop/buy-iphone/iphone-15-pro'
+    const user = {
+        name: "Frank C.",
+        surname: "Goldman",
+        position: "Sales Executive",
+        phone: "+009 567 14586",
+        email: "info@company.com",
+        website: "www.yourwebsite.com",
+        address: "1234 Shinn Street New York, N48"
     };
-    res.render('index', { phone });
+    
+    res.render('index', user);
 });
 
 const PORT = 3000;
