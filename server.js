1b815cbc2d07fa1c26b82f80cb20db750fdea4a9
kush-ankit <er.ankitkush@gmail.com>
2025-02-11 01:19:46 +0530

    phone card

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('view engine', 'ejs'); // Set EJS as the template engine
-app.set('views', path.join(__dirname, 'views')); // Set the views directory
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Welcome', message: 'Hello from EJS!' });
+    const phone = {
+        name: 'iPhone 15 Pro Max',
+        price: '1199',
+        image: 'https://www.silicon.co.uk/wp-content/uploads/2023/09/Apple-iPhone-15-Pro-lineup-02.jpg',
+        buyLink: 'https://www.apple.com/shop/buy-iphone/iphone-15-pro'
+    };
+    res.render('index', { phone });
 });
 
 const PORT = 3000;
